8d36d57d4dfe956e05710bfcf8c2d9431f5b5bcc
Tools <Tools@ideyalabs.com>
2023-12-06 15:15:24 +0000

    Updated generate_report.bat

--- generate_report.bat
+++ generate_report.bat
@@ -1,5 +1,5 @@
-node get-executions.js
+node src/get-executions.js
 node src/parse-execution.js
-node extractFailedTestIDs
+node src/extractFailedTestIDs.js
 node src/build-json.js
 marge -i=true -p "Hubsync Regression Run" -t "Hubsync Regression Run" report/Hubsync_test_run_report.json
