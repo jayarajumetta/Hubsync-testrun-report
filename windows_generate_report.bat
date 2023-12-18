50c97caa75cf9fc0c7f97fee62dbc35378b06913
Jayaraju Metta <JayarajuM@ideyalabs.com>
2023-12-18 14:48:28 +0530

    corrected windows execution file

--- windows_generate_report.bat
+++ windows_generate_report.bat
@@ -1,8 +1,8 @@
 set branch="master"
 set execution="End to End-All-Sequential / Regression2-Sequential"
 
-node src/get-executions.js -branch:"%branch%" -execution:"%execution%"
-node src/parse-execution.js  -execution:"%execution%"
+node src/get-executions.js -branch:%branch% -execution:%execution%
+node src/parse-execution.js  -execution:%execution%
 node src/extractFailedTestIDs.js
 node src/build-json.js
 marge -i=true -p "Hubsync Regression Run" -t "Hubsync Regression Run" report/Hubsync_test_run_report.json -o ./html-report
